32c341a4e1a5ff19dbe9f63efca54632bf394ca7
MASTER_SABBIR <sabbir117824@gmail.com>
2023-02-10 05:26:20 +0600

    initial commit

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//var name= "sabbir"
+//var name2,city= "hossan","chittagong"
+//fmt.Println(name,name2,city)
+var c rune= 'A'
+var age int= 24
+var result float32= 545.54
+fmt.Println(c,age,result)
+}
